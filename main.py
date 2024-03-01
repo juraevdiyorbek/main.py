0057f978148b3bf0a82588ff3b395001dc905d88
Diyorbek Juraev <85333075+juraevdiyorbek@users.noreply.github.com>
2024-03-01 10:13:09 +0500

    Update main.py
    print list

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ def maxProduct(nums) -> int:
         nums.sort()
         return (nums[-1]-1)*(nums[-2]-1)
 nums = [1,4,2,3,6,3]
+#listni o'zini ekranga chiqaradi
+print(nums)
 print(maxProduct(nums))
